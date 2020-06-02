13d9e192542ade668427a54164c68af51973b855
Ishan Sharma <ishandeveloper@outlook.com>
2020-06-03 00:36:14 +0530

    Data

---
+++ data.js
@@ -0,0 +1,60 @@
+const data = {
+  experience: [
+    {
+      post: "Technical Author",
+      company: "Cypher Eruption",
+      link: "https://cyphereruption.net",
+      dates: "August 2014 - Sept 2016",
+      span: "2 Years",
+      stack: "VB.Net, Visual C#, Wordpress",
+      description: `Cypher Eruption is a free place to learn best programming languages and ethical hacking. Be a learner and learn programming and ethical hacking or be a teacher and teach anything unique. We respect every single visitor, author, contributor. Because of you we are here.`,
+    },
+    {
+      post: "Frontend Developer",
+      company: "Developer Students Clubs, Chitkara University",
+      link: "#",
+      dates: "October 2019 - Present",
+      span: "8 Months",
+      stack: "HTML5, CSS, JavaScript, jQuery, Flutter",
+      description: `Developer Student Clubs are university based community groups for students interested in Google developer technologies. Students from all undergraduate or graduate programs with an interest in growing as a developer are welcome. By joining a DSC, students grow their knowledge in a peer-to-peer learning environment and build solutions for local businesses and their community.
+
+`,
+    },
+  ],
+  achievements: [
+    {
+      title: "3rd Rank",
+      type: "award",
+      css: "left: -8px",
+      fa: "fa-trophy",
+      linkTitle: "in Google Techstars Startup Weekend",
+      link: " ",
+    },
+    {
+      title: "1st Rank",
+      type: "award",
+      fa: "fa-trophy",
+      linkTitle: "In Website Development Competition",
+      link: " ",
+      link2Title: "held at GNA University",
+      link2: " ",
+    },
+    {
+      title: "Hotspoter crossed 5 Million+ Downloads",
+      type: "ANALYTICS",
+      fa: "fa-star",
+      linkTitle: "Softonic",
+      link: "https://hotspoter.en.softonic.com",
+      link2Title: "CNET",
+      link2: "http://bit.ly/CNETHotspoter",
+    },
+    {
+      title: "Chatter App got appreciated by Flutter Team",
+      type: "appreciation",
+      fa: "fa-twitter",
+      linkTitle: "Tweet",
+      link: "https://bit.ly/ChatterTweet",
+    },
+  ],
+};
+
